c66a71cf25f52ebb21032e9760fc7ee30b32bccc
ufsgit <ufstechgithub@gmail.com>
2024-10-25 11:07:26 +0530

    loading issued fixed bundle uploaded

--- lib/http/http_urls.dart
+++ lib/http/http_urls.dart
@@ -1,8 +1,8 @@
 class HttpUrls {
-   static String baseUrl = 'http://3.89.129.51:3514';
+  //  static String baseUrl = 'http://3.89.129.51:3514';
  
   
-// static String baseUrl = 'http://34.228.212.237:3514'; //  new live
+static String baseUrl = 'http://34.228.212.237:3514'; //  new live
 
 //   static String baseUrl = 'https://7413-103-214-232-132.ngrok-free.app';
 
